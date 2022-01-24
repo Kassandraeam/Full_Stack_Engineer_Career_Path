b05c24fefc18e33987f84cac70918e2222566310
Kassandraeam <31658826+Kassandraeam@users.noreply.github.com>
2022-01-24 13:44:56 -0600

    Update Dog_Years.js

--- Dog_Years.js
+++ Dog_Years.js
@@ -12,8 +12,3 @@ myAgeInDogYears = earlyYears + laterYears;
 let myName = 'Kassy'.toLowerCase();
 
 console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
-```
-
-This would print:
-
-My name is kassy. I am 27 years old in human years which is 121 years old in dog years.
