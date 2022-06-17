de876b1e4cd7775ea34b03ef39bc8e05e853244a
Kas <31658826+Kassandraeam@users.noreply.github.com>
2022-06-17 16:15:59 -0500

    Update ManyBugs.js

--- ManyBugs.js
+++ ManyBugs.js
@@ -5,11 +5,11 @@ timeStyle: 'short',
 });
 
 class Bug {
-    constructor(weaponDevName, weaponName, ATVIID, CL, CSS, console, tester, shipTestDev){
+    constructor(weaponDevName, weaponName, redacted, CL, CSS, console, tester, shipTestDev){
 
         this._weaponDevName = weaponDevName;
         this._weaponName = weaponName;
-        this._ATVIID = ATVIID;
+        this._redacted = redacted;
         this._CL = CL;
         this._CSS = CSS;
         this._console = console;
@@ -22,8 +22,8 @@ class Bug {
     get weaponName(){
         return this._weaponName;
     }
-    get ATVIID(){
-        return this._ATVIID;
+    get redacted(){
+        return this._redacted;
     }
     get CL(){
         return this._CL;
@@ -48,25 +48,25 @@ SUMMARY:
 
 DESCRIPTION: 
 
-When the User selects and equips the ${this.weaponName}, and views the Unlock Criteria for the Diamond and Atomic camo, they will observe that the tracking bar is inconsistent with the written goal text.
+When the User selects and equips the ${this.weaponName}, and views the {redacted} for the {redacted} and {redacted}, they will observe that the {redacted} is inconsistent with the written goal text.
 
 *Steps to Reproduce:*
 
-# Launch WZ.
-# Select a Private Match.
+# Launch {redacted}.
+# Select a {redacted}.
 # Edit Loadout.
-# Equip the ${this.weaponName} and then select Gunsmith.
-# Click the Customize tab, and then the Camouflage option.
+# Equip the ${this.weaponName} and then select {redacted}.
+# Click the {redacted}, and then the {redacted} option.
 # Select {redacted}.
-# Observe that the tracking bar is inconsistent with the written goal text.
+# Observe that the {redacted} is inconsistent with the {redacted} text.
 
 *Results:*
 
-Observe that the tracking bar is inconsistent with the written goal text.
+Observe that the {redacted} is inconsistent with the {redacted} text.
 
 *Notes:*
 
-* Profile: ${this.ATVIID}
+* Profile: ${this.redacted}
 * Time Stamp: ${pst + ' PST'}
 * Online/LAN: Online
 * CL: ${this.CL}
@@ -82,42 +82,42 @@ ${this.console} - Verified Open on CL# ${this.CL} by ${this.tester}
 * Regressed on ${this.shipTestDev}
 * ${this.CSS}
 * Repro: 5/5
-* Profile: ${this.ATVIID}
+* Profile: ${this.redacted}
 `)};
 }    
 class PC extends Bug{
-    constructor(weaponDevName, weaponName, ATVIID, tester, shipTestDev){
-        super(weaponDevName, weaponName, ATVIID, 'Build Name', 'CSS number', 'PC', tester, shipTestDev); 
+    constructor(weaponDevName, weaponName, redacted, tester, shipTestDev){
+        super(weaponDevName, weaponName, redacted, 'Build Name', 'CSS number', 'PC', tester, shipTestDev); 
         return this.description(), this.cc();
     }
 }
 class playstation extends Bug{
-    constructor(weaponDevName, weaponName, ATVIID, tester,shipTestDev){
-        super(weaponDevName, weaponName, ATVIID, 'Build Name', 'CSS number', 'PS4', tester, shipTestDev); 
+    constructor(weaponDevName, weaponName, redacted, tester,shipTestDev){
+        super(weaponDevName, weaponName, redacted, 'Build Name', 'CSS number', 'PS4', tester, shipTestDev); 
         return this.description(), this.cc();
     }
 }
 class xbox extends Bug{
-    constructor(weaponDevName, weaponName, ATVIID, tester,shipTestDev){
-        super(weaponDevName, weaponName, ATVIID, 'Build Name', 'CSS number', 'XBOX', tester, shipTestDev); 
+    constructor(weaponDevName, weaponName, redacted, tester,shipTestDev){
+        super(weaponDevName, weaponName, redacted, 'Build Name', 'CSS number', 'XBOX', tester, shipTestDev); 
         return this.description(), this.cc();
     }
 }
 
 class commentCC extends Bug{
-    constructor(ATVIID, CL, CSS, tester, shipTestDev){
-        super(ATVIID, CL, CSS, tester, shipTestDev); 
+    constructor(redacted, CL, CSS, tester, shipTestDev){
+        super(redacted, CL, CSS, tester, shipTestDev); 
         return this.cc();
     }
 }
 
 // PC USERS HERE: remove the two back slashes before const.
-const PCs = new PC('Weapon dev name', 'Weapon name', 'qamseawolf15#9626484', 'KEam', 'Ship');
+const PCs = new PC('Weapon dev name', 'Weapon name', 'UserID', 'KEam', 'Ship');
 
 // SONY USERS HERE: remove the two back slashes before const.
-//const Sony = new playstation('Weapon dev name', 'Weapon name', 'Qam_bowfin_1343', 'PRobertson', 'Ship');
+//const Sony = new playstation('Weapon dev name', 'Weapon name', 'UserID', 'UserName', 'Ship');
 
 // MICROSOFT USERS HERE: remove the two back slashes before const.
-//const Xbox = new xbox('Weapon Dev Name', 'Weapon Name', 'ATVI ID', 'TEST CC Abbreviation', 'Ship, Dev, or Test');
+//const Xbox = new xbox('Weapon Dev Name', 'Weapon Name', 'UserID', 'TEST CC Abbreviation', 'Ship, Dev, or Test');
 
-// const Console = new PC/playstation/xbox('Weapon Dev Name', 'Weapon Name', 'ATVI ID', 'TEST CC Abbreviation', 'Ship, Dev, or Test')
+// const Console = new PC/playstation/xbox('Weapon Dev Name', 'Weapon Name', 'UserID', 'TEST CC Abbreviation', 'Ship, Dev, or Test')
