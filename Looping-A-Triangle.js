9084c5c45b16c22c6f21d13c74cd0096c00c2470
Kas Eam <KassandraEam@gmail.com>
2022-06-20 20:09:08 -0500

    completed

---
+++ Looping-A-Triangle.js
@@ -0,0 +1,19 @@
+/*
+Write a loop that makes seven calls to console.log to output the following triangle
+#
+##
+###
+####
+#####
+######
+#######
+
+//initalizer, stopping condition, iteration
+
+I want it to run once, then twice, then thrice.
+*/
+
+for (let i = "#"; i.length < 8; i += "#"){
+    console.log(i);
+};
+
