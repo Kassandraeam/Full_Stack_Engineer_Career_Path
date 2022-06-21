0caa5c4fd59d296bbf09c7a0ddb4ba86b46512ad
Kas Eam <KassandraEam@gmail.com>
2022-06-20 21:01:57 -0500

    Completed FizzBuzz

--- Fizzbuzz.js
+++ Fizzbuzz.js
@@ -6,10 +6,28 @@ Print "fizzbuzz" for numbers that are divisble by both 3 and 5.
 so we want to iterate through numbers 1-100. i.isInteger
 */
 
+
+// for (let counter = 1; counter <= 100; counter ++){
+//     if (counter % 3 === 0) {
+//         console.log('Fizz');
+//     } else if (counter % 5 === 0) {
+//         console.log('Buzz');
+//     } else if (counter % 3 === 0 && counter % 5 === 0){
+//         console.log('FizzBuzz');
+//     } else {
+//         console.log(counter);
+//     }
+// };
+
 for (let counter = 1; counter <= 100; counter ++){
-    if (counter % 3 === 0) {
-        console.log('Fizz') //if I put return here, it stops the loop.
+    if (counter % 3 === 0 && counter % 5 === 0){
+        console.log('FizzBuzz');
+    } else if (counter % 3 === 0) {
+        console.log('Fizz');
     } else if (counter % 5 === 0) {
         console.log('Buzz');
+    } else {
+        console.log(counter);
     }
-}; 
+};
+
